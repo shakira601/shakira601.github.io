2f1e36b41f811cfdb9cc4fbdeef05154b1cd250b
shakira601 <84365454+shakira601@users.noreply.github.com>
2024-01-18 23:20:22 +0545

    Add files via upload

---
+++ Module-4/SpeakGoodBye.js
@@ -0,0 +1,13 @@
+(function(window){
+  var speakWord= "Good Bye";
+  
+  var byeSpeaker={
+
+    speak: function (name) {
+      console.log(speakWord + " " + name);
+    }
+  };
+  
+window.byeSpeaker=byeSpeaker;
+})(window);
+
